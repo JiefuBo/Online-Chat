d24b678a352a10939af05bf0f86ae55f3f3ce41c
jamesbo <nmg15949415070@163.com>
2024-06-21 21:17:38 +1000

    更新了read me

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5002, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5003, debug=True)
